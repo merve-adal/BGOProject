5a63a5ca73d7da1d9d52c59d08cc1e5a22aa877f
berkayturkoglu <bturkoglu361@gmail.com>
2024-08-28 21:45:07 +0300

    Proje Demosu

--- Assets/CardManager.cs
+++ Assets/CardManager.cs
@@ -1,131 +1,149 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardManager : MonoBehaviour
+public class ChoiceSystem : MonoBehaviour
 {
-    // UI Referanslarý
-    public Text storyText;
-    public Button leftButton;
-    public Button rightButton;
+    public Text scenarioText;
+    public Button acceptButton;
+    public Button rejectButton;
+    public Image powerCircle;
+    public Image techCircle;
+    public Image moneyCircle;
+    public Image successCircle;
 
-    // Kategori Barlarý ve Textleri
-    public Image powerBar;
     public Text powerText;
-    public Image technologyBar;
-    public Text technologyText;
-    public Image moneyBar;
+    public Text techText;
     public Text moneyText;
-    public Image successBar;
     public Text successText;
 
-    // Kategori Deðerleri
-    private int power = 50;
-    private int technology = 50;
-    private int money = 50;
-    private int success = 50;
+    private int powerValue = 50;
+    private int techValue = 50;
+    private int moneyValue = 50;
+    private int successValue = 50;
 
-    // Senaryo ve Seçim Verileri
-    private string[] scenarios = {
-        "Bir hacker sana gizli bir veritabaný sunuyor. Bu veritabaný, mega þirketlerin en üst düzey yöneticilerinin karanlýk sýrlarýný içeriyor.",
-        "Karanlýk bir sokakta bir bilim insaný seni durduruyor ve sana yasa dýþý bir teknoloji satmak istiyor.",
-        "Bir mega þirket, sana büyük bir rüþvet teklif ediyor, ancak bu teklifi kabul etmek itibarýni sarsabilir.",
-        "Bir teknoloji konferansýna davet edildin. Orada konuþmacý olman, sektörle ilgili itibarýný artýrabilir.",
-        "Siber bir saldýrý ile mega bir þirketin güvenlik sistemine sýzma fýrsatýn var."
-    };
-
-    private string[][] choices = {
-        new string[] {"Reddet", "Kabul Et"},
-        new string[] {"Reddet", "Satýn Al"},
-        new string[] {"Reddet", "Kabul Et"},
-        new string[] {"Gitme", "Konuþ"},
-        new string[] {"Geri Çekil", "Sýz"}
-    };
-
-    // Her seçim için kategori deðiþiklikleri: Reddet (Ýlk 4), Kabul Et (Son 4)
-    private int[][] outcomes = {
-        new int[] {0, -20, -10, 0, 0, 20, 10, 0},  // Senaryo 1
-        new int[] {0, -20, 0, 0, 0, 30, -10, 0},  // Senaryo 2
-        new int[] {-20, 0, 0, -20, 0, 0, 30, 10},  // Senaryo 3
-        new int[] {0, 0, 0, -10, 0, 0, 0, 20},  // Senaryo 4
-        new int[] {0, 10, 0, -10, 10, 20, 0, -10}  // Senaryo 5
-    };
-
-    // Þu anki senaryo indeksi
     private int currentScenario = 0;
+
+    private string[] scenarios = {
+        "Bir hacker grubunun þehirdeki güvenlik sistemlerini devre dýþý býraktýðýný öðrendiniz. Onlarý durdurmak için müdahale etmeli misiniz?",
+        "Büyük bir þirket size þehirdeki en yüksek binayý kontrol etme teklifinde bulunuyor. Teklifi kabul edecek misiniz?",
+        "Þehirdeki eski bir teknoloji uzmaný, size gizli bir buluþ teklif ediyor. Bu buluþu kullanmalý mýsýnýz?",
+        "Bir suç lordu, size büyük miktarda para karþýlýðýnda þehirdeki bir bölgeyi kontrol etmeyi teklif ediyor. Kabul edecek misiniz?",
+        "Bir grup asi, þehirde bir ayaklanma baþlatmayý planlýyor. Onlara yardým etmeli misiniz?"
+    };
 
     void Start()
     {
-        UpdateUI();
-
-        // Butonlara týklama olaylarýný ekle
-        leftButton.onClick.AddListener(() => MakeChoice(0));
-        rightButton.onClick.AddListener(() => MakeChoice(1));
+        UpdateCircleValues();
+        DisplayScenario();
+        acceptButton.onClick.AddListener(() => MakeChoice(true));
+        rejectButton.onClick.AddListener(() => MakeChoice(false));
     }
 
-    void MakeChoice(int choiceIndex)
+    void DisplayScenario()
     {
-        // Seçim sonucu deðiþecek deðerler
-        int powerChange = 0;
-        int technologyChange = 0;
-        int moneyChange = 0;
-        int successChange = 0;
+        scenarioText.text = scenarios[currentScenario];
+    }
 
-        // Seçime göre deðerleri belirle
-        if (choiceIndex == 0) // Sol seçim (Reddi Et)
+    void MakeChoice(bool accepted)
+    {
+        switch (currentScenario)
         {
-            powerChange = outcomes[currentScenario][0];
-            technologyChange = outcomes[currentScenario][1];
-            moneyChange = outcomes[currentScenario][2];
-            successChange = outcomes[currentScenario][3];
+            case 0:
+                if (accepted)
+                {
+                    ModifyCategories(ref powerValue, powerCircle, powerText, 10);
+                    ModifyCategories(ref techValue, techCircle, techText, -10);
+                }
+                else
+                {
+                    ModifyCategories(ref techValue, techCircle, techText, 10);
+                    ModifyCategories(ref powerValue, powerCircle, powerText, -10);
+                }
+                break;
+
+            case 1:
+                if (accepted)
+                {
+                    ModifyCategories(ref moneyValue, moneyCircle, moneyText, 10);
+                    ModifyCategories(ref powerValue, powerCircle, powerText, -10);
+                }
+                else
+                {
+                    ModifyCategories(ref successValue, successCircle, successText, 10);
+                    ModifyCategories(ref moneyValue, moneyCircle, moneyText, -10);
+                }
+                break;
+
+            case 2:
+                if (accepted)
+                {
+                    ModifyCategories(ref techValue, techCircle, techText, 10);
+                    ModifyCategories(ref successValue, successCircle, successText, -10);
+                }
+                else
+                {
+                    ModifyCategories(ref powerValue, powerCircle, powerText, 10);
+                    ModifyCategories(ref techValue, techCircle, techText, -10);
+                }
+                break;
+
+            case 3:
+                if (accepted)
+                {
+                    ModifyCategories(ref moneyValue, moneyCircle, moneyText, 10);
+                    ModifyCategories(ref successValue, successCircle, successText, -10);
+                }
+                else
+                {
+                    ModifyCategories(ref powerValue, powerCircle, powerText, 10);
+                    ModifyCategories(ref moneyValue, moneyCircle, moneyText, -10);
+                }
+                break;
+
+            case 4:
+                if (accepted)
+                {
+                    ModifyCategories(ref successValue, successCircle, successText, 10);
+                    ModifyCategories(ref powerValue, powerCircle, powerText, -10);
+                }
+                else
+                {
+                    ModifyCategories(ref techValue, techCircle, techText, 10);
+                    ModifyCategories(ref successValue, successCircle, successText, -10);
+                }
+                break;
         }
-        else if (choiceIndex == 1) // Sað seçim (Kabul Et)
+
+        currentScenario++;
+        if (currentScenario < scenarios.Length)
         {
-            powerChange = outcomes[currentScenario][4];
-            technologyChange = outcomes[currentScenario][5];
-            moneyChange = outcomes[currentScenario][6];
-            successChange = outcomes[currentScenario][7];
+            DisplayScenario();
         }
-
-        // Deðerleri güncelle
-        power += powerChange;
-        technology += technologyChange;
-        money += moneyChange;
-        success += successChange;
-
-        // Deðerleri sýnýrla
-        power = Mathf.Clamp(power, 0, 100);
-        technology = Mathf.Clamp(technology, 0, 100);
-        money = Mathf.Clamp(money, 0, 100);
-        success = Mathf.Clamp(success, 0, 100);
-
-        // Konsolda hangi deðerin nasýl deðiþtiðini kontrol et
-        Debug.Log($"Güç: {powerChange}, Teknoloji: {technologyChange}, Para: {moneyChange}, Baþarý: {successChange}");
-        Debug.Log($"Güncel Deðerler -> Güç: {power}, Teknoloji: {technology}, Para: {money}, Baþarý: {success}");
-
-        // UI'yý güncelle
-        UpdateUI();
-
-        // Bir sonraki senaryoya geç
-        currentScenario = (currentScenario + 1) % scenarios.Length;
+        else
+        {
+            scenarioText.text = "Demo Bitti!";
+            acceptButton.gameObject.SetActive(false);
+            rejectButton.gameObject.SetActive(false);
+        }
     }
 
-    void UpdateUI()
+    void ModifyCategories(ref int categoryValue, Image categoryCircle, Text categoryText, int amount)
     {
-        // Senaryoyu ve seçimleri güncelle
-        storyText.text = scenarios[currentScenario];
-        leftButton.GetComponentInChildren<Text>().text = choices[currentScenario][0];
-        rightButton.GetComponentInChildren<Text>().text = choices[currentScenario][1];
-
-        // Barlarý ve textleri güncelle
-        UpdateBarAndText(moneyBar, moneyText, money, "");
-        UpdateBarAndText(powerBar, powerText, power, "");
-        UpdateBarAndText(technologyBar, technologyText, technology, "");
-        UpdateBarAndText(successBar, successText, success, "");
+        categoryValue = Mathf.Clamp(categoryValue + amount, 0, 100);
+        categoryCircle.fillAmount = categoryValue / 100f;
+        categoryText.text = categoryValue.ToString();
     }
 
-    void UpdateBarAndText(Image bar, Text text, int value, string categoryName)
+    void UpdateCircleValues()
     {
-        bar.fillAmount = value / 100f;
-        text.text = $"{categoryName} {value}";
+        powerText.text = powerValue.ToString();
+        techText.text = techValue.ToString();
+        moneyText.text = moneyValue.ToString();
+        successText.text = successValue.ToString();
+
+        powerCircle.fillAmount = powerValue / 100f;
+        techCircle.fillAmount = techValue / 100f;
+        moneyCircle.fillAmount = moneyValue / 100f;
+        successCircle.fillAmount = successValue / 100f;
     }
 }
